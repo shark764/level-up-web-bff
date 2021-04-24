72dd8dae679e070bf39a90ad4b102efac4e639f8
Kevin Damas <kevin.damas@focusservices.com>
2021-04-24 01:40:48 +0000

    Adding facilities routes

--- src/modules/facilities/routes.js
+++ src/modules/facilities/routes.js
@@ -3,20 +3,145 @@ import { error, success } from './../../utils/response';
 import Facilities from './model';
 const router = Router();
 
+//Get all Facilities
 router.get('/facilities', async (req, res) => {
   try {
     const facilities = await Facilities.find();
-    return res
-      .status(200)
-      .json(success({ requestId: req.id, data: { facilities } }));
-  } catch (err) {
-    res.status(500).json(error({ requestId: req.id, code: 500, message: err }));
+    return res.status(200).json(
+      success({
+        requestId: req.id,
+        data: {
+          facilities,
+        },
+      })
+    );
+  } catch (error) {
+    res.status(500).json(
+      error({
+        requestId: req.id,
+        code: 500,
+        message: error,
+      })
+    );
   }
 });
 
-// eslint-disable-next-line require-await
-router.post('/facilities', async (req, res) =>
-  res.status(200).json(success({ requestId: req.id, data: 'TODO' }))
+//Get a Facility based on id
+router.get('/facilities/:id', async (req, res) => {
+  try {
+    const facility = await Facilities.findById(req.params.id);
+    if (!facility)
+      return res
+        .status(404)
+        .json(error({ requestId: req.id, code: 404, message: 'Not Found' }));
+
+    res.json(facility);
+  } catch (err) {
+    return res
+      .status(500)
+      .json(error({ requestId: req.id, code: 500, message: err }));
+  }
+});
+
+//Add a Facility
+router.post('/facilities', async (req, res) => {
+  try {
+    const addedFacility = await Facilities.create({
+      name: req.body.name,
+      description: req.body.description,
+      address: req.body.address,
+      location: req.body.location,
+      schedule: req.body.schedule,
+      phoneNumber: req.body.phoneNumber,
+      pictures: req.body.pictures,
+      amenities: req.body.amenities,
+    });
+
+    return res
+      .status(200)
+      .json(success({ requestId: req.id, data: { addedFacility } }));
+  } catch (err) {
+    return res
+      .status(500)
+      .json(error({ requestId: req.id, code: 500, message: err }));
+  }
+});
+
+//Update facility
+router.patch('/facilities/:id', (req, res) => {
+  try {
+    Facilities.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true, omitUndefined: true },
+      (err, updatedFacility) => {
+        if (err) {
+          return res
+            .status(500)
+            .json(error({ requestId: req.id, code: 500, message: err }));
+        } else if (!updatedFacility) {
+          return res.status(404).json(
+            error({
+              requestId: req.id,
+              code: 404,
+              message: 'Facility not found',
+            })
+          );
+        } else {
+          return res.status(200).json(
+            success({
+              requestId: req.id,
+              data: { facility: updatedFacility },
+            })
+          );
+        }
+      }
+    );
+  } catch (err) {
+    console.error(err);
+    return res
+      .status(500)
+      .json(error({ requestId: req.id, code: 500, message: err }));
+  }
+});
+
+router.delete('/facilities/:id', (req, res) => {
+  try {
+    Facilities.findByIdAndDelete(req.params.id, (err, deletedFacility) => {
+      if (err) {
+        return res
+          .status(500)
+          .json(error({ requestId: req.id, code: 500, message: err }));
+      } else if (!deletedFacility) {
+        return res.status(404).json(
+          error({
+            requestId: req.id,
+            code: 404,
+            message: 'Facility not found',
+          })
+        );
+      } else {
+        return res
+          .status(200)
+          .json(
+            success({ requestId: req.id, data: { facility: deletedFacility } })
+          );
+      }
+    });
+  } catch (err) {
+    console.error(err);
+    return res
+      .status(500)
+      .json(error({ requestId: req.id, code: 500, message: err }));
+  }
+});
+
+router.all('/facilities', (req, res) =>
+  res
+    .status(405)
+    .json(
+      error({ requestId: req.id, code: 405, message: 'Method not allowed' })
+    )
 );
 
 export default router;
